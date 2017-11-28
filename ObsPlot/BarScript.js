2ec118c056b6362bb0bc3de94915f8e394f50339
Irene Mayor <ihmayor@ucalgary.ca>
2017-11-27 20:25:06 -0700

    Category Selection Completed

--- ObsPlot/BarScript.js
+++ ObsPlot/BarScript.js
@@ -8,20 +8,33 @@
     "": "Total"
 }
 
+var categorySelection = "RACE"
+var areaSelection = "US";
 
+var colMapping = {
+    "GEN": "Gender",
+    "EDU": "Education",
+    "OVR": "Total",
+    "AGEYR": "AgeYear",
+    "RACE": "RaceEthnicity",
+    "INC": "Income",
+    "": "Total"
+}
+var selectedNestedData = [];
 
+var dataLoaded;
+var nestedData;
 $("#SelectBar").change(function (sb) {
     var selectedValue = sb.target.value;
-    console.log(barMapping[selectedValue]);
+    var newCat = barMapping[selectedValue];
+    categorySelection = newCat;
+    $(".barChart").remove();
+    update();
 });
 
-d3.csv("AllSurvVal.csv", function (d, i) {
-    d.Data_Value = +d.Data_Value;
-    return d;
-},
-function (error, data) {
-    if (error) throw error;
 
+function update()
+{
     var svg = d3.select("svg");
 
     var margin = { top: 20, right: 20, bottom: 70, left: 90 }
@@ -32,23 +45,137 @@ function (error, data) {
     var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
         y = d3.scaleLinear().rangeRound([height, 0]);
 
-
-
-    var g = svg.append("g")
+    var g = svg.append("g").attr("class","barChart")
         .attr("transform", "translate(" + (1320 - 300) + "," + 490 + ")scale(0.35)");
 
-    var categorySelection = "RACE"
-    var areaSelection = "US";
+    g.append("text")
+        .attr("fill", "black")
+        .text(areaSelection + " Obesity Rate per " + colMapping[categorySelection] + " per Year")
+        .style("font-size", "40px")
+        .attr("x", "20")
+        .attr("y", "-30")
 
-    var colMapping = {
-        "GEN": "Gender",
-        "EDU": "Education",
-        "OVR": "Total",
-        "AGEYR": "AgeYear",
-        "RACE": "RaceEthnicity",
-        "INC": "Income",
-        "": "Total"
-    }
+    var colKeys = Object.keys(colMapping);
+    selectedNestedData = nestedData[categorySelection];
+    var innerCategories = Object.keys(selectedNestedData);
+    var color = d3.scaleOrdinal(d3.schemeAccent);
+    var selectedData = dataLoaded.filter((d) => {
+        return d.StratificationCategoryId1 == categorySelection && d.LocationAbbr == areaSelection;
+    })
+
+
+    ///LEGEND INFO
+    var barLegend = svg.append("g").attr("class","barChart").attr("transform", "translate(" + (990) + "," + 490 + ")");
+    var nestedLegend = d3.nest().key(function (d) { return d.Simple; }).object(selectedData);
+
+    Object.keys(nestedLegend).forEach((cat, i) => {
+        var perRow = 4;
+        var offset = (i % perRow) * 45;
+        var mappedCol = colMapping[categorySelection]
+        var fullName = nestedLegend[cat][0][mappedCol];
+        var x = -170 + offset;
+        var y = 85 + 50 * (Math.floor((i / perRow)) - 1);
+
+        barLegend.append("rect")
+            .attr("fill", color(i))
+            .attr('x', x)
+            .attr('y', y)
+            .attr('height', 20)
+            .attr('width', 20)
+            .on("mouseover", (d) => {
+                var htmlFull = fullName
+                showToolTipBar(htmlFull, 0, 0, 25, 115);
+            })
+            .on("mouseleave", (x) => {
+                div.transition()
+                .style('opacity', 0)
+                .duration(500)
+            })
+
+        barLegend.append("text")
+           .attr("fill", "black")
+            .attr('x', x)
+            .attr('y', y + 30)
+            .attr("font-size", '8px')
+            .text(cat)
+    })
+
+    //Axis
+    x.domain(selectedData.map((d) => { return d.YearStart; }));
+
+    y.domain([0, d3.max(selectedData, (d) => { return d.Data_Value; })]);
+
+    g.append("g")
+        .call(d3.axisBottom(x))
+        .attr("transform", "translate(0," + height + ")")
+        .style("font-size", "20px")
+        .append("text")
+        .attr('y', 70)
+        .attr('x', 400)
+        .attr('fill', "black")
+        .text("Year Start/End")
+        .style('font-size', '25px')
+
+
+    g.append("g")
+        .call(d3.axisLeft(y).ticks(10))
+        .style("font-size", "20px")
+        .append("text")
+        .attr('transform', 'rotate(-90)')
+        .attr('y', -65)
+        .attr('x', (-1 * height / 2) + 150)
+        .attr('fill', "black")
+        .text("Obesity Level")
+        .style('font-size', '25px')
+
+    //Bar Chart Actual Bars
+    g.selectAll(".bar")
+      .data(selectedData)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .style("fill", (d) => {
+            for (var i = 0; i < innerCategories.length; i++) {
+                if (d[colMapping[categorySelection]] == innerCategories[i])
+                    return color(i);
+            }
+        })
+        .attr("x", function (d) {
+            var barW = x.bandwidth() / (innerCategories.length * 2.5);
+            for (var i = 0; i < innerCategories.length; i++) {
+                if (d[colMapping[categorySelection]] == innerCategories[i])
+                    return x(d.YearStart) + ((barW + barW / 2) * i) + x.bandwidth() / 4;
+            }
+        })
+        .attr("y", function (d) { return y(d.Data_Value); })
+        .attr("width", x.bandwidth() / (innerCategories.length * 2.5))
+        .attr("height", function (d) { return height - y(d.Data_Value); })
+        .on("mouseleave", function (d) {
+        })
+        .on("mouseover", function (d) {
+
+        })
+}
+
+d3.csv("AllSurvVal.csv", function (d, i) {
+    d.Data_Value = +d.Data_Value;
+    return d;
+},
+function (error, data) {
+    if (error) throw error;
+    dataLoaded = data;
+    var svg = d3.select("svg");
+
+    var margin = { top: 20, right: 20, bottom: 70, left: 90 }
+
+    var width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+        y = d3.scaleLinear().rangeRound([height, 0]);
+
+    var g = svg.append("g").attr("class", "barChart")
+        .attr("transform", "translate(" + (1320 - 300) + "," + 490 + ")scale(0.35)");
+
 
     g.append("select")
 
@@ -60,7 +187,7 @@ function (error, data) {
         .attr("y", "-30")
 
     var colKeys = Object.keys(colMapping);
-    var nestedData = d3.nest()
+    nestedData = d3.nest()
               .key(function (d) { return d.StratificationCategoryId1; })
               .key(function (d) {
                   for (var k = 0; k < colKeys.length; k++) {
@@ -75,7 +202,7 @@ function (error, data) {
               .rollup(function (v) { return d3.sum(v, function (d) { return d.Data_Value; }); })
               .object(data);
 
-    var selectedNestedData = nestedData[categorySelection];
+     selectedNestedData = nestedData[categorySelection];
     var innerCategories = Object.keys(selectedNestedData);
     var color = d3.scaleOrdinal(d3.schemeAccent);
     var selectedData = data.filter((d) => {
@@ -84,7 +211,7 @@ function (error, data) {
 
 
     ///LEGEND INFO
-    var barLegend = svg.append("g").attr("transform", "translate(" + (990) + "," + 490 + ")");
+    var barLegend = svg.append("g").attr("class","barChart").attr("transform", "translate(" + (990) + "," + 490 + ")");
     var nestedLegend = d3.nest().key(function (d) { return d.Simple; }).object(selectedData);
 
     Object.keys(nestedLegend).forEach((cat, i) => {
